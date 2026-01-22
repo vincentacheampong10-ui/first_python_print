0f6ff20d704ccd1e90e448bdfe87a983f0225154
Vincentacheampong <Vincentacheampong10@gmail.com>
2026-01-22 07:19:42 -0500

    type_casting

--- first_project.py
+++ first_project.py
@@ -1,38 +1,19 @@
-# Variables
+# Typecasting = Converting a variable from one data type to another
 
-# Strings
-first_name  = "Vincent"
-food = "Pizza"
-print(f"Hello {first_name}")
-print(f"Do you like {food} ?")
-
-# Integers
+name = "Vincent"
 age = 25
-quantity = 3
-number_of_students = 30
+gpa = 3.3
+is_student = True
 
-print(f"You are {age} years old")
-print(f"You are buying {quantity} items")
-print(f"You have {number_of_students} students")
+print(type(is_student))
 
-# Float
+gpa = (int(gpa))
+print(gpa)
 
-price = 10.99
-gpa = 3.2
-distance = 5.5
+age = str(age)
 
-print(f"The price is {price}")
-print(f"Your gpa is {gpa}")
-print(f"You ran {distance} miles yesterday")
+age += "1"
+print(age)
 
-# Boolean
-
-is_student = False
-for_sale = False
-if_online = True
-
-
-if is_student:
-    print("You are a student")
-else:
-    print("You are not a student")
+name = bool(name)
+print(name)

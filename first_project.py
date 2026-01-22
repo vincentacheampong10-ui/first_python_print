b12db634eb4de19b63febe4b772db9a408b341bc
Vincentacheampong <Vincentacheampong10@gmail.com>
2026-01-22 07:03:27 -0500

    variables

--- first_project.py
+++ first_project.py
@@ -1,5 +1,38 @@
-# Comments can not be seen.
+# Variables
 
-print("Hello World")
-print("My name is Vincent")
-print("I like pizza")
+# Strings
+first_name  = "Vincent"
+food = "Pizza"
+print(f"Hello {first_name}")
+print(f"Do you like {food} ?")
+
+# Integers
+age = 25
+quantity = 3
+number_of_students = 30
+
+print(f"You are {age} years old")
+print(f"You are buying {quantity} items")
+print(f"You have {number_of_students} students")
+
+# Float
+
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+print(f"The price is {price}")
+print(f"Your gpa is {gpa}")
+print(f"You ran {distance} miles yesterday")
+
+# Boolean
+
+is_student = False
+for_sale = False
+if_online = True
+
+
+if is_student:
+    print("You are a student")
+else:
+    print("You are not a student")

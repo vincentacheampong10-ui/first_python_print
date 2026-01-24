21cce42b32511ad3b7a1a4670e726cbc2de43f3e
Vincentacheampong <Vincentacheampong10@gmail.com>
2026-01-24 09:50:41 -0500

    Logical operators = OR, AND, NOT

--- first_project.py
+++ first_project.py
@@ -1,22 +1,26 @@
-# Python Calculator
-import math
+# Logical operators = OR, AND, NOT
 
-num1 = float(input("Enter the first Number: "))
-num2 = float(input("Enter the second Number: "))
+temp = 25
+is_raining = False
 
-operator = input("Choose an operator(+ - * /): ")
 
-if operator == "+":
-    result = num1 + num2
-    print(round(result))
-elif operator == "-":
-    result = num1 - num2
-    print(round(result))
-elif operator == "*":
-    result = num1 * num2
-    print(round(result))
-elif operator == "/":
-    result = num1 / num2
-    print(round(result, 2))
-else:
-    print(f"The {operator} is not Valid")
+# OR
+# if temp > 35 or temp < 0 or is_raining:
+#     print("The outdoor event has been cancelled")
+# else:
+#     print("The outdoor event is still scheduled")
+
+
+# AND
+# if temp < 35 and is_raining:
+#     print("The outdoor event has been cancelled")
+# else:
+#     print("The outdoor event is still scheduled")
+
+# Not
+if temp < 35 and not is_raining:
+    print("It's cold outside today ")
+elif 65 < temp < 75:
+    print("Tt is warm outside")
+elif 75 > temp <95:
+    print("It is very hot outside")

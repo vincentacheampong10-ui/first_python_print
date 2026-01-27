8e6fa0960abfe0109e7308e1878a364ba8b453cb
Vincentacheampong <Vincentacheampong10@gmail.com>
2026-01-27 06:38:12 -0500

    While loops food input

--- first_project.py
+++ first_project.py
@@ -1,9 +1,19 @@
 # While loops
 
-name = input("What is your name: ")
+# age = int(input("How old are you: "))
+#
+# while age < 0:
+#     print("Age cannot be negative ")
+#     age = int(input("How old are you: "))
+#
+#     print(F"You are {age} years old")
 
-while name == "":
-    print("You did not enter your name")
-    name = input("What is your name: ")
+food = input("Enter your favourite food(q to quit): ")
 
-    print(F"Hello {name}")
+while not food == "q":
+    print(f"Your favourite food is {food.capitalize()}.")
+    food = input("Enter your favourite food(q to quit): ")
+
+print("Bye, have a good day.")
+
+
